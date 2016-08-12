a54e375ba9643267ec858086b7732848948e9f9b
brusinow <brusinow@me.com>
2016-08-12 00:40:28 -0700

    hard coded show page is working, social nav added to footer

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,13 +3,18 @@ angular.module('SistersCtrls', ['SistersServices'])
 .controller('HomeCtrl', ['$scope', '$state','$timeout','LoadedService', function($scope, $state, $timeout, LoadedService) {
     $scope.loaded = LoadedService.get();
     console.log("what is loaded? ",$scope.loaded);
+    var width = window.innerWidth;
+    console.log("inner width: ",width);
       
-    if (!$scope.loaded){ 
+    if (!$scope.loaded && width > 806){ 
     $timeout(function () {
           console.log("fade in home!!!!");
           $scope.fadeHome = true;
           LoadedService.set(true);
     }, 600);
+    } else {
+      $scope.fadeHome = true;
+      LoadedService.set(true);
     } 
 }])
 
@@ -22,7 +27,15 @@ angular.module('SistersCtrls', ['SistersServices'])
 }]) 
 
 .controller('NavCtrl', ['$scope','$timeout', function($scope, $timeout){
+    $scope.toggle = true;
+    var width = window.innerWidth;
+    console.log("inner width: ",width);
+
+    if (width > 806){
     $timeout(function () {
-        $scope.fade = true;
+      $scope.fade = true;
     }, 100);
+    } else {
+      $scope.fade = true;
+    }
 }])

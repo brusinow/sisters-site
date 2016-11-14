78ece444f25ad10e6055519f3d58ca3349dd2088
brusinow <brusinow@me.com>
2016-11-13 21:22:44 -0800

    still having issues

--- app/js/controllers/06 show_controller.js
+++ app/js/controllers/06 show_controller.js
@@ -1,12 +1,11 @@
 angular.module('SistersCtrls')
 
 .controller('ShowsCtrl', ['$scope', '$state','currentAuth','$uibModal','$log','$firebaseArray','moment','Auth','getShows', function($scope, $state, currentAuth, $uibModal,$log, $firebaseArray, moment, Auth, getShows){
-  console.log("entering show controller");
-  var main = document.getElementById("main");
-  main.style.backgroundColor = 'rgba(255, 255, 255, .9)';
+var main = document.getElementById("main");
+  main.style.backgroundColor = '';
   main.style.width = '';
   main.style.padding = '';
-  $scope.$emit('loadMainContainer', 'loaded');
+$scope.$emit('loadMainContainer', 'loaded');
   $scope.shows = getShows;
 
   $scope.auth = Auth;
